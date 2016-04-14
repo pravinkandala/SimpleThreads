0f05b7aa90951cf022a506cf6500f4d7525a903e
Pravin Kandala <pravinkandala@hotmail.com>
2016-04-14 08:55:37 -0500

    Done.

--- app/src/main/java/edu/niu/cs/z1761257/simplethreads/MainActivity.java
+++ app/src/main/java/edu/niu/cs/z1761257/simplethreads/MainActivity.java
@@ -1,13 +1,73 @@
 package edu.niu.cs.z1761257.simplethreads;
 
+import android.app.Activity;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import org.w3c.dom.Text;
+
+import android.os.Handler;
+import java.util.logging.LogRecord;
+
+public class MainActivity extends Activity {
+
+    private TextView countTV;
+    private Integer counter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+        counter = 0;
+
+        countTV = (TextView)findViewById(R.id.counterTextView);
+
+        Thread thread = new Thread(countRunnable);
+
+        thread.start();
+
+
+    }//end of onCreate
+
+    @Override
+    protected void onStart() {
+
+        super.onStart();
+
+        counter = 0;
+
+    }//end of onStart
+
+
+    private Runnable countRunnable = new Runnable() {
+        @Override
+        public void run() {
+
+            while(true){
+                counter++;
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                threadHandler.sendEmptyMessage(0);
+            }
+
+        }
+    };//end of countRunnable
+
+
+    public Handler threadHandler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+         //   super.handleMessage(msg);
+            countTV.setText(counter.toString());
+        }
+    };//end of threadHandler
+
+
+}//end of MainActivity
