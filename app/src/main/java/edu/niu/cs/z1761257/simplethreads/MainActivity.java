7e1f358df10960d51064efff27aea33f71ce2ec0
Pravin Kandala <pravinkandala@hotmail.com>
2016-04-14 08:40:13 -0500

    Initial commit

---
+++ app/src/main/java/edu/niu/cs/z1761257/simplethreads/MainActivity.java
@@ -0,0 +1,13 @@
+package edu.niu.cs.z1761257.simplethreads;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
